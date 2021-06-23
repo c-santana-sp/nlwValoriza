fd2087a3fb3cc38235cdd5ae6876c6c0dbde5731
c-santana-sp <carlos.santana@bandtec.com.br>
2021-06-22 21:24:51 -0300

    Adding ensureAdmin middleware and setting it in routes

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -5,12 +5,18 @@ import { UserService } from "../services/UserService"
 export class UserController {
 
   async handle(req: Request, res: Response) {
-    const { name, email, admin } = req.body;
 
-    const userService = new UserService();
+    try {
+      const { name, email, admin } = req.body;
 
-    const user = await userService.execute({name, email, admin});
-    return res.json(user);
+      const userService = new UserService();
+
+      const user = await userService.execute({name, email, admin});
+      return res.json(user);
+    } catch(err) {
+      return res.status(400).json({ error: err.message })
+    }
+
   }
 
 }
